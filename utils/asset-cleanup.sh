6f090c6c46e95716f65797a2936d1ec51a35d0df
Sebastian Kessler <s.kessler@tryface.de>
2022-08-03 09:51:31 +0200

    build: add cleaner as static file

--- utils/asset-cleanup.sh
+++ utils/asset-cleanup.sh
@@ -7,20 +7,7 @@ main() {
 # Prune exif data
 exif_cleaner() {
 
-    # Get exif tool
-    DIR="./tmp/exiftool/"
-    if ! [ -d "$DIR" ]; then
-        mkdir -p ./tmp/exiftool/
-        wget https://exiftool.org/Image-ExifTool-12.44.tar.gz
-        gzip -dc Image-ExifTool-12.44.tar.gz | tar -xf -
-        mv Image-ExifTool-12.44/* tmp/exiftool/
-        rm Image-ExifTool-12.44.tar.gz
-        rm -r Image-ExifTool-12.44
-        cd tmp/exiftool
-    fi
-
-    # Run metadata removal
-    ./exiftool -all:all= -r ../../src
+    ./bin/exiftool -all:all= -r ../src
 
     echo "All metadata removed!"
 }
